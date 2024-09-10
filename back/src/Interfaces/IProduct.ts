4e968ee6829b96b907b43a50fcface12572c8392
B9inzon <tatopinzon369@gmail.com>
2024-09-10 18:43:28 -0500

    Vite proyect created

---
+++ back/src/Interfaces/IProduct.ts
@@ -0,0 +1,21 @@
+export interface IProduct{
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    stock: number;
+    image: string;
+    categoryId: string;
+}
+
+
+
+
+// export interface IAppointment{
+//     id:number,
+//     date:string,
+//     time:string,
+//     userId:number,
+//     status: AppointmentSatus,
+//     description: string
+// }
