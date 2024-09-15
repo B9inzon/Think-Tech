0ab31f8d7c82b371e2b1cf294210ad1c2d34f7d7
B9inzon <tatopinzon369@gmail.com>
2024-09-15 13:55:43 -0500

    Home page

--- front/src/views/Home/HomePage.tsx
+++ front/src/views/Home/HomePage.tsx
@@ -1,9 +1,12 @@
+import ProductsList from "@/components/ProductsList/ProductsList";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Welcome!</h1>
+      <hr />
+      <ProductsList />
     </div>
   );
 };
