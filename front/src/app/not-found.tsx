f1c2dc328bdc969d74c32030812374157e4d5a7d
B9inzon <tatopinzon369@gmail.com>
2024-09-28 19:23:33 -0500

    404 page

--- front/src/app/not-found.tsx
+++ front/src/app/not-found.tsx
@@ -1,11 +1,26 @@
-import Link from 'next/link'
- 
+import Link from "next/link";
+
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>No se pudo encontrar la página solicitada</p>
-      <Link href="/">Regresar a Home</Link>
+    <div className="flex flex-col-reverse lg:flex-row content-center items-center justify-center gap-5 h-full w-full p-7">
+      {/* Contenedor de texto */}
+      <div className="flex flex-col w-full lg:w-[50%] items-center justify-center p-2 ">
+        <p className="font-bold text-2xl lg:text-3xl mb-4 lg:mb-8">
+          Lo sentimos, no hemos encontrado la página que busca.
+        </p>
+        <Link href="/" className="underline text-lg lg:text-xl">
+          Volver a la página principal
+        </Link>
+      </div>
+
+      {/* Contenedor de imagen */}
+      <div className="flex items-center justify-center w-full lg:w-[40%] p-5 lg:p-10 ">
+        <img
+          src="/assets/png2.png"
+          alt="unplugged"
+          className="w-full h-auto lg:h-[100%]"
+        />
+      </div>
     </div>
-  )
+  );
 }
