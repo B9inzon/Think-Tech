cab47682f249adfb2830498192c1c70ad212a03e
B9inzon <tatopinzon369@gmail.com>
2024-10-01 09:29:55 -0500

    Add products

--- back/src/helpers/preLoadProducts.ts
+++ back/src/helpers/preLoadProducts.ts
@@ -23,6 +23,26 @@ const productsToPreLoad: IProduct[] = [
     stock: 10,
   },
   {
+    name: "iPhone 16 pro",
+    price: 699,
+    description:
+      "Experience power and elegance with the iPhone 11: capture stunning moments with its dual-camera system, enjoy exceptional performance, and immerse yourself in a brilliant Liquid Retina display. Discover a world of possibilities in the palm of your hand!",
+    image:
+      "https://www.apple.com/v/iphone/home/bw/images/overview/select/iphone_16pro__erw9alves2qa_xlarge.png",
+    categoryId: 1,
+    stock: 10,
+  },
+  {
+    name: "iPhone 16 pro",
+    price: 699,
+    description:
+      "Experience power and elegance with the iPhone 11: capture stunning moments with its dual-camera system, enjoy exceptional performance, and immerse yourself in a brilliant Liquid Retina display. Discover a world of possibilities in the palm of your hand!",
+    image:
+      "https://www.apple.com/v/iphone/home/bw/images/overview/select/iphone_16pro__erw9alves2qa_xlarge.png",
+    categoryId: 1,
+    stock: 10,
+  },
+  {
     name: "MacBook Air",
     price: 999,
     description:
@@ -30,6 +50,46 @@ const productsToPreLoad: IProduct[] = [
     image:
       "https://http2.mlstatic.com/D_NQ_NP_648428-MLA46516517286_062021-O.webp",
     categoryId: 2,
+    stock: 10,
+  },
+  {
+    name: "MacBook Air",
+    price: 999,
+    description:
+      "Embrace efficiency and sophistication with the MacBook Air: lightweight design meets powerful performance, stunning Retina display brings your work to life, and all-day battery life keeps you productive wherever you go. Elevate your computing experience with the MacBook Air.",
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_648428-MLA46516517286_062021-O.webp",
+    categoryId: 2,
+    stock: 10,
+  },
+  {
+    name: "MacBook Air",
+    price: 999,
+    description:
+      "Embrace efficiency and sophistication with the MacBook Air: lightweight design meets powerful performance, stunning Retina display brings your work to life, and all-day battery life keeps you productive wherever you go. Elevate your computing experience with the MacBook Air.",
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_648428-MLA46516517286_062021-O.webp",
+    categoryId: 2,
+    stock: 10,
+  },
+  {
+    name: "iPad Pro",
+    price: 799,
+    description:
+      "Unleash your creativity and productivity with the iPad Pro: powerful performance, stunning Liquid Retina display, and all-day battery life make the iPad Pro the perfect tool for work and play. Transform your ideas into reality with the iPad Pro.",
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_994384-MLU77738266205_072024-O.webp",
+    categoryId: 3,
+    stock: 10,
+  },
+  {
+    name: "iPad Pro",
+    price: 799,
+    description:
+      "Unleash your creativity and productivity with the iPad Pro: powerful performance, stunning Liquid Retina display, and all-day battery life make the iPad Pro the perfect tool for work and play. Transform your ideas into reality with the iPad Pro.",
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_994384-MLU77738266205_072024-O.webp",
+    categoryId: 3,
     stock: 10,
   },
   {
@@ -53,6 +113,46 @@ const productsToPreLoad: IProduct[] = [
     stock: 10,
   },
   {
+    name: "Apple Watch Series 6",
+    price: 399,
+    description:
+      "Stay connected and healthy with the Apple Watch Series 6: track your workouts, monitor your health, and stay in touch with the people and information you care about most. Experience the future of health and wellness with the Apple Watch Series 6.",
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_816100-MLA72067667918_102023-O.webp",
+    categoryId: 4,
+    stock: 10,
+  },
+  {
+    name: "Apple Watch Series 6",
+    price: 399,
+    description:
+      "Stay connected and healthy with the Apple Watch Series 6: track your workouts, monitor your health, and stay in touch with the people and information you care about most. Experience the future of health and wellness with the Apple Watch Series 6.",
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_816100-MLA72067667918_102023-O.webp",
+    categoryId: 4,
+    stock: 10,
+  },
+  {
+    name: "AirPods Pro 2",
+    price: 249,
+    description:
+      "Immerse yourself in sound with the AirPods Pro: active noise cancellation, transparency mode, and customizable fit make the AirPods Pro the perfect companion for music, calls, and everything in between. Elevate your audio experience with the AirPods Pro.",
+    image:
+      "https://www.apple.com/v/airpods-max/g/images/overview/contrast/airpods_pro_white__e7dtczxw2lkm_large.png",
+    categoryId: 5,
+    stock: 10,
+  },
+  {
+    name: "AirPods Pro 2",
+    price: 249,
+    description:
+      "Immerse yourself in sound with the AirPods Pro: active noise cancellation, transparency mode, and customizable fit make the AirPods Pro the perfect companion for music, calls, and everything in between. Elevate your audio experience with the AirPods Pro.",
+    image:
+      "https://www.apple.com/v/airpods-max/g/images/overview/contrast/airpods_pro_white__e7dtczxw2lkm_large.png",
+    categoryId: 5,
+    stock: 10,
+  },
+  {
     name: "AirPods Pro 2",
     price: 249,
     description:
@@ -70,6 +170,46 @@ const productsToPreLoad: IProduct[] = [
     image:
       "https://http2.mlstatic.com/D_NQ_NP_800774-MLA45740145234_042021-O.webp",
     categoryId: 6,
+    stock: 10,
+  },
+  {
+    name: "HomePod mini",
+    price: 99,
+    description:
+      "Elevate your home audio experience with the HomePod mini: immersive sound, intelligent assistant, and smart home hub make the HomePod mini the perfect addition to your home. Enjoy a world of music, news, and more with the HomePod mini.",
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_800774-MLA45740145234_042021-O.webp",
+    categoryId: 6,
+    stock: 10,
+  },
+  {
+    name: "HomePod mini",
+    price: 99,
+    description:
+      "Elevate your home audio experience with the HomePod mini: immersive sound, intelligent assistant, and smart home hub make the HomePod mini the perfect addition to your home. Enjoy a world of music, news, and more with the HomePod mini.",
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_800774-MLA45740145234_042021-O.webp",
+    categoryId: 6,
+    stock: 10,
+  },
+  {
+    name: "IMac",
+    price: 2214,
+    description:
+      "El mejor computador todo-en-uno del mundo, ahora con los superpoderes del chip M3. Su gran pantalla de 24 pulgadas1 te da todo el espacio que necesitas, y su irresistible diseño llena de vida cualquier lugar. Míralo bien: el iMac es perfecto para trabajar y jugar.",
+    image:
+      "https://www.apple.com/v/imac/p/images/overview/color_front_green__eb8qbnemmre6_large.jpg",
+    categoryId: 7,
+    stock: 10,
+  },
+  {
+    name: "IMac",
+    price: 2214,
+    description:
+      "El mejor computador todo-en-uno del mundo, ahora con los superpoderes del chip M3. Su gran pantalla de 24 pulgadas1 te da todo el espacio que necesitas, y su irresistible diseño llena de vida cualquier lugar. Míralo bien: el iMac es perfecto para trabajar y jugar.",
+    image:
+      "https://www.apple.com/v/imac/p/images/overview/color_front_green__eb8qbnemmre6_large.jpg",
+    categoryId: 7,
     stock: 10,
   },
   {
